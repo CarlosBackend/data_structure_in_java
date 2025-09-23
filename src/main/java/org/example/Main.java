c03cce0acbabfacd8e1d93f4f795a2e8ff48cbd0
Carlos Henrique <carlinhosbackend@gmail.com>
2025-09-23 15:00:27 -0300

    feat:

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,12 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+       Vetores v = new Vetores(3);
+       v.adiciona("B");
+       v.adiciona("C");
+       v.adiciona("D");
+       v.adiciona("E");
+       v.adiciona("F");
+       v.adiciona("G");
     }
 }
